4d4692c66325725570cd60df1722b30af63492cc
andreaoquendo <oquendo.3455@gmail.com>
2023-11-14 15:34:40 -0300

    edit notes

--- Mini3/Defaults/Font.swift
+++ Mini3/Defaults/Font.swift
@@ -9,9 +9,14 @@ import Foundation
 import SwiftUI
 
 extension Font {
-    static let appTitle = Font.custom("Poppins-Light", size: 40)
-    static let appBoldTitle = Font.custom("Poppins-Medium", size: 40)
+    static let appTitle = Font.custom("Poppins-Light", size: 36)
+    static let appBoldTitle = Font.custom("Poppins-Medium", size: 36)
     static let appSubTitle = Font.custom("Poppins-Regular", size: 24)
+    static let regularTextButton = Font.custom("Poppins-Regular", size: 18)
+    static let highlighterCount = Font.custom("SF Pro Text", size: 13.6)
+    
+    static let notesTitle = Font.custom("Poppins-Medium", size: 34)
+    static let notesSubTitle = Font.custom("Poppins", size: 12)
 //    static let appTitle = Font.custom("DMSerifDisplay-Regular", size: 24)
 //    static let appQuoteSubtitle = Font.custom("Baskervville-Regular", size: 12)
 //    
