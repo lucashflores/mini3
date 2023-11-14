2cef6af19f90f33e592eb7dd151bfedc0ea01d32
andreaoquendo <oquendo.3455@gmail.com>
2023-11-13 23:06:27 -0300

    feat: ui list of points

---
+++ Mini3/Defaults/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  Mini3
+//
+//  Created by Andrea Oquendo on 13/11/23.
+//
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
---
+++ Mini3/Defaults/Font.swift
@@ -0,0 +1,8 @@
+//
+//  Font.swift
+//  Mini3
+//
+//  Created by Andrea Oquendo on 13/11/23.
+//
+
+import Foundation
