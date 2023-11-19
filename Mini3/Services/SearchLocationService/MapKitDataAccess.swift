734bea4ad0322696913db3397815ce236f40b5cc
lucashflores <lucas.henrique.flores2001@gmail.com>
2023-11-19 19:23:49 -0300

    fixed map search

--- Mini3/Services/SearchLocationService/MapKitDataAccess.swift
+++ Mini3/Services/SearchLocationService/MapKitDataAccess.swift
@@ -4,8 +4,7 @@ class MapKitDataAccess: DataAccess {
     func fetch(request: MapSearchRequest) async throws -> [MKMapItem] {
         let mapKitRequest = MKLocalSearch.Request()
         mapKitRequest.naturalLanguageQuery = request.query
-        mapKitRequest.pointOfInterestFilter = .init(including: [.restaurant, .bakery, .brewery, .cafe, .winery, .foodMarket, .hotel])
-        mapKitRequest.resultTypes = .pointOfInterest
+        mapKitRequest.resultTypes = [.pointOfInterest, .address]
         let search = MKLocalSearch(request: mapKitRequest)
         
         let response = try await search.start()
